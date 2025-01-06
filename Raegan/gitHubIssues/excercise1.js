991906b7e21e0468fe4c93e774491fa741e8ec58
McCallum <32571@nelnet.net>
2025-01-05 20:14:39 -0600

    adding Issues-Exercise1

---
+++ Raegan/gitHubIssues/excercise1.js
@@ -0,0 +1,6 @@
+function ageInDaysCalc() {
+    let ageInYears = prompt("Please enter your current age in years", "");
+    let ageInDays = ageInYears * 365;
+    console.log("You are approximately " + ageInDays + " days old!");
+
+}
